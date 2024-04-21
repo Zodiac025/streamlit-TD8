30ad5f94b1dc9a44414c8aa7eedeadecd647d87e
Harsh Meshram <156357440+Zodiac025@users.noreply.github.com>
2024-04-21 18:16:13 +0530

    Create streamlit-TD8.py

---
+++ streamlit-TD8.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.write("""
+Largest Number
+""")
+
+
+def user_input_features():
+  Number1=st.number_input("Enter the first Number")
+  Number2=st.number_input("Enter the second Number")
+  Number3=st.number_input("Enter the third Number")
+  return (max(Number1,Number2,Number3))
+
+a=user_input_features()
+
+st.subheader("The largest number of the above is",a)
